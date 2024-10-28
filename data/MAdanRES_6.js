af636ad4ff77bad838fffb17554bf1b1ebd1f780
arianuntono5 <151104704+arianuntono5@users.noreply.github.com>
2024-10-28 22:24:14 +0700

    Add files via upload

---
+++ data/MAdanRES_6.js
@@ -0,0 +1,11 @@
+var json_MAdanRES_6 = {
+"type": "FeatureCollection",
+"name": "MAdanRES_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMA": "RESERVOAR", "DESA": "Pancur Wening", "MATA_AIR": "RES Ngawen", "ZONA": "Zona Res.Ngawen", "DMA": "RES Ngawen", "THN_PASANG": "2018", "FUNGSI": null, "TGL_PASANG": "8 - Januari" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 109.916923394851949, -7.370557564818645 ] ] } },
+{ "type": "Feature", "properties": { "NAMA": "MATA AIR SIGAJAH", "DESA": "Bomerto", "MATA_AIR": "Sigajah", "ZONA": "Zona Sigajah", "DMA": "Distribusi JDU Sigajah NON DMA", "THN_PASANG": "2018", "FUNGSI": "Mata Air", "TGL_PASANG": "8 - Januari" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 109.951234718473103, -7.350153877945192 ] ] } },
+{ "type": "Feature", "properties": { "NAMA": "MATA AIR SIBENDO", "DESA": "Bomerto", "MATA_AIR": "Sibendo", "ZONA": "Zona Distribusi Sibendo", "DMA": "Non DMA Distribusi Sibendo", "THN_PASANG": "2018", "FUNGSI": "Mata Air", "TGL_PASANG": "8 - Januari" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 109.93018389752099, -7.349976992910369 ] ] } },
+{ "type": "Feature", "properties": { "NAMA": "MATA AIR KALIYOGA", "DESA": "Bomerto", "MATA_AIR": "Sigajah", "ZONA": "Zona Sigajah", "DMA": "Distribusi JDU Sigajah NON DMA", "THN_PASANG": "2018", "FUNGSI": "Mata Air", "TGL_PASANG": "8 - Januari" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 109.968907607095701, -7.348081473947843 ] ] } }
+]
+}
